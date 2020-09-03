c61199054c086db2af6f388c5247c4df1aee6c01
Anmay Kumar <anmayk1997@gmail.com>
2020-09-03 15:14:49 +0530

    Some changes

---
+++ js/data.js
@@ -0,0 +1,71 @@
+// dummy data
+
+
+
+export var data = {
+    "entity": "collection",
+    "count": 3,
+    "items": [
+        {
+            "id": "inv_FXrfj1VFug8iMF",
+            "entity": "invoice",
+            "receipt": null,
+            "invoice_number": null,
+            "customer_id": "cust_FXrcTrGv8u2NpC",
+            "customer_details": {
+                "id": "cust_FXrcTrGv8u2NpC",
+                "name": "Sonam4 Gupta",
+                "email": "gupta@razorpay.com",
+                "contact": "9090909019",
+                "gstin": null,
+                "billing_address": null,
+                "shipping_address": null,
+                "customer_name": "Sonam4 Gupta",
+                "customer_email": "gupta@razorpay.com",
+                "customer_contact": "9090909019"
+            },
+            
+        },
+        {
+            "id": "inv_FXfLqmKmx3gaP3",
+            "entity": "invoice",
+            "receipt": null,
+            "invoice_number": null,
+            "customer_id": "cust_FXeyTugYOgPBKi",
+            "customer_details": {
+                "id": "cust_FXeyTugYOgPBKi",
+                "name": "Sonam Gupta",
+                "email": "sonam@razorpay.com",
+                "contact": "9900990099",
+                "gstin": null,
+                "billing_address": null,
+                "shipping_address": null,
+                "customer_name": "Sonam Gupta",
+                "customer_email": "sonam@razorpay.com",
+                "customer_contact": "9900990099"
+            },
+        },
+        {
+            "id": "inv_FXf5Z31fOG2eho",
+            "entity": "invoice",
+            "receipt": null,
+            "invoice_number": null,
+            "customer_id": "cust_FXeyTugYOgPBKi",
+            "customer_details": {
+                "id": "cust_FXeyTugYOgPBKi",
+                "name": "Sonam Gupta",
+                "email": "sonam@razorpay.com",
+                "contact": "9900990099",
+                "gstin": null,
+                "billing_address": null,
+                "shipping_address": null,
+                "customer_name": "Sonam Gupta",
+                "customer_email": "sonam@razorpay.com",
+                "customer_contact": "9900990099"
+            },
+            
+        }
+    ]
+};
+
+
